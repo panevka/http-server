cc019d797cb7a6e485499be582b34df2dcd1965d
panevka <97028404+panevka@users.noreply.github.com>
2025-07-27 01:02:57 +0200

    feat: establish connection and send a message

--- main.c
+++ main.c
@@ -3,14 +3,21 @@
 #include <string.h>
 #include <sys/socket.h>
 
-#define PORT "3490"
+#define LISTEN_BACKLOG 10
+#define PORT "3000"
 #define PROTOCOL_NAME "tcp"
 
 int main(void) {
-  int sock; // socket
+  int sock, new_sock; // socket
+  struct sockaddr_storage new_addr;
+  socklen_t addr_size;
+
   int binding;
   struct protoent *protocolptr; // pointer to a protocol
   struct addrinfo hints, *res;
+
+  char *msg = "wazzup";
+  int len, bytes_sent;
 
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_INET;
@@ -23,5 +30,14 @@ int main(void) {
   sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
   binding = bind(sock, res->ai_addr, res->ai_addrlen);
+  connect(sock, res->ai_addr, res->ai_addrlen);
+  listen(sock, LISTEN_BACKLOG);
+
+  addr_size = sizeof new_addr;
+  new_sock = accept(sock, (struct sockaddr *)&new_addr, &addr_size);
+
+  len = strlen(msg);
+  bytes_sent = send(new_sock, msg, len,  0);
+
   return EXIT_SUCCESS;
 }
