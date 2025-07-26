33f569f87eb74074d28d8665e7bf0aec7d7b6094
panevka <97028404+panevka@users.noreply.github.com>
2025-07-27 00:17:37 +0200

    feat: add socket call

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <netdb.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+
+#define PROTOCOL_NAME "tcp"
 
 int main(void) {
   int sock; // socket
   struct protoent* protocolptr; // pointer to a protocol
 
   protocolptr = getprotobyname(PROTOCOL_NAME);
+  sock = socket(AF_INET, SOCK_STREAM, protocolptr->p_proto);
 
   return EXIT_SUCCESS;
 }
