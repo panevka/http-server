22e8446e142dadfcf07267ff60cffdd2fa21cdc6
panevka <97028404+panevka@users.noreply.github.com>
2025-07-27 00:42:37 +0200

    feat: add socket binding

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include <netdb.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 
+#define PORT "3490"
 #define PROTOCOL_NAME "tcp"
 
 int main(void) {
   int sock; // socket
-  struct protoent* protocolptr; // pointer to a protocol
+  int binding;
+  struct protoent *protocolptr; // pointer to a protocol
+  struct addrinfo hints, *res;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // fill ip automatically
+
+  getaddrinfo(NULL, PORT, &hints, &res);
 
   protocolptr = getprotobyname(PROTOCOL_NAME);
-  sock = socket(AF_INET, SOCK_STREAM, protocolptr->p_proto);
+  sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
+  binding = bind(sock, res->ai_addr, res->ai_addrlen);
   return EXIT_SUCCESS;
 }
