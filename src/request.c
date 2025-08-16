47c4f77bb5bc63b1da03ed77aa8d4f06f036f8d5
panevka <97028404+panevka@users.noreply.github.com>
2025-08-16 14:25:20 +0200

    feat: add space for null terminator in request buffer

--- src/request.c
+++ src/request.c
@@ -114,7 +114,7 @@ char *read_file(char *path) {
 }
 
 void handle_request(int sock) {
-  char buffer[MAX_REQUEST_SIZE];
+  char buffer[MAX_REQUEST_SIZE + 1];
   ssize_t received_size = read(sock, buffer, sizeof(buffer));
   if (received_size > 0) {
     buffer[received_size] = '\0';
