9b0a225c1676a62e8361f5b2ddbcd3470b55c75a
panevka <97028404+panevka@users.noreply.github.com>
2025-08-19 11:43:50 +0200

    feat: create function for path sanitization to prevent path traversal attacks

--- src/request.c
+++ src/request.c
@@ -101,6 +101,28 @@ int starts_with(const char *string, const char *prefix) {
   return 1;
 }
 
+int sanitize_path(const char *safe_base_path, const char *unsafe_path,
+                  char sanitized_path[MAX_FILE_PATH_LENGTH + 1]) {
+
+  char temp[strlen(safe_base_path) + strlen(unsafe_path) + 1];
+  char resolved_path[MAX_FILE_PATH_LENGTH + 1];
+
+  snprintf(temp, 1, "%s%s", safe_base_path, unsafe_path);
+
+  if (realpath(temp, resolved_path) == NULL) {
+    fprintf(stderr, "Received path could not be resolved: %s. %s \n", temp,
+            strerror(errno));
+    return -1;
+  }
+
+  if (starts_with(temp, safe_base_path)) {
+    memcpy(sanitized_path, resolved_path, MAX_FILE_PATH_LENGTH);
+    return 0;
+  }
+
+  return -1;
+}
+
 char *create_headers(size_t body_length) {
   const char headers[] = "HTTP/1.1 200 OK\r\n"
                          "Content-Type: text/html\r\n"
