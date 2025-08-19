74c935c2106a556c5a332f69e6a873e42523463b
panevka <97028404+panevka@users.noreply.github.com>
2025-08-19 18:00:31 +0200

    refactor: move macros & struct definitions from header to source

--- src/request.c
+++ src/request.c
@@ -8,6 +8,20 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define MAX_METHOD_LENGTH 3
+#define MAX_URI_LENGTH 8000
+#define MAX_PROTOCOL_LENGTH 20
+#define MAX_REQUEST_SIZE 8192
+
+#define MAX_FILE_PATH_LENGTH 4096
+#define MAX_FILE_SIZE 1048576
+
+struct request_start_line {
+  char method[MAX_METHOD_LENGTH + 1];
+  char uri[MAX_URI_LENGTH + 1];
+  char protocol[MAX_PROTOCOL_LENGTH + 1];
+};
+
 /**
  * @brief Parses the start line of an HTTP request.
  *
