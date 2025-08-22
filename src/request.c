42a4466d1128f0b5efe598a76fdb22eda9bec35e
panevka <97028404+panevka@users.noreply.github.com>
2025-08-22 13:21:28 +0200

    feat: create helper function for error printing

--- src/request.c
+++ src/request.c
@@ -1,5 +1,7 @@
 #include "request.h"
 #include <errno.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,6 +23,31 @@ struct request_start_line {
   char uri[MAX_URI_LENGTH + 1];
   char protocol[MAX_PROTOCOL_LENGTH + 1];
 };
+
+typedef enum { MSG_WARNING, MSG_ERROR } msg_type_t;
+
+__attribute__((format(printf, 3, 4))) void
+perror_msg(msg_type_t type, bool with_errno, const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+
+  // Print severity prefix
+  if (type == MSG_WARNING)
+    fprintf(stderr, "Warning: ");
+  else if (type == MSG_ERROR)
+    fprintf(stderr, "Error: ");
+
+  // Print the user's formatted message
+  vfprintf(stderr, fmt, args);
+  va_end(args);
+
+  // Optionally append strerror(errno)
+  if (with_errno) {
+    fprintf(stderr, ": %s", strerror(errno));
+  }
+
+  fprintf(stderr, "\n");
+}
 
 /**
  * @brief Parses the start line of an HTTP request.
