89ab32809f5edd51f06ac1dce0f7eb985cfc51d1
panevka <97028404+panevka@users.noreply.github.com>
2025-08-23 11:40:24 +0200

    refactor: move socke initialization function to separate file

---
+++ include/socket.h
@@ -0,0 +1,6 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+int initialize_socket(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,72 +1,13 @@
 #include "request.h"
+#include "socket.h"
 #include <netdb.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-
-#define LISTEN_BACKLOG 10
-#define PORT "3000"
-#define PROTOCOL_NAME "tcp"
-
-int initialize_socket(void) {
-
-  int sock;
-  struct addrinfo hints, *res, *p;
-
-  int rv;
-  int yes = 1;
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE; // fill ip automatically
-
-  if ((rv = getaddrinfo(NULL, PORT, &hints, &res)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    return -1;
-  }
-
-  for (p = res; p != NULL; p = p->ai_next) {
-    if ((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-      perror("server: socket");
-      continue;
-    }
-
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-      perror("setsockopt");
-      return -1;
-    }
-
-    if (bind(sock, p->ai_addr, p->ai_addrlen) == -1) {
-      close(sock);
-      perror("server: bind");
-      continue;
-    }
-
-    break;
-  }
-
-  freeaddrinfo(res);
-
-  if (p == NULL) {
-    fprintf(stderr, "server: failed to bind\n");
-    return -1;
-  }
-
-  if (listen(sock, LISTEN_BACKLOG) == -1) {
-    perror("listen");
-    return -1;
-  }
-
-  printf("server: waiting for connections...\n");
-
-  return sock;
-}
 
 int main(void) {
   int socket_fd, new_socket;
