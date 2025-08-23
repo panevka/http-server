98b22c740f51c6383213beffabce1721a31fefc3
panevka <97028404+panevka@users.noreply.github.com>
2025-08-23 13:27:15 +0200

    feat: improve functionality of logging
    List of changes:
    - output stream type assigned to reusable variable
    - errno saved on function beggining to prevent overwrites by vprintf
    - if statements chain modified to switch statement
    - localtime_r used instead of localtime to prevent multithreading issues
    in the future

--- src/logging.c
+++ src/logging.c
@@ -8,38 +8,57 @@
 
 __attribute__((format(printf, 3, 4))) void
 log_msg(msg_type_t type, bool with_errno, const char *fmt, ...) {
+  int saved_errno = errno;
 
   va_list args;
   va_start(args, fmt);
+  char log_prefix[100];
+  FILE *output_stream;
 
   char without_ms[64];
   char with_ms[64];
   struct timeval tv;
+  struct tm tm_buf;
   struct tm *tm;
 
-  gettimeofday(&tv, NULL);
-  if ((tm = localtime(&tv.tv_sec)) != NULL) {
-    strftime(without_ms, sizeof(without_ms), "%Y-%m-%d %H:%M:%S.%%06u %z", tm);
-    snprintf(with_ms, sizeof(with_ms), without_ms, tv.tv_usec);
-    fprintf(stdout, "[%s] ", with_ms);
+  switch (type) {
+  case MSG_INFO:
+    snprintf(log_prefix, sizeof(log_prefix), "%s", "Info: ");
+    output_stream = stdout;
+    break;
+  case MSG_WARNING:
+    snprintf(log_prefix, sizeof(log_prefix), "%s", "Warning: ");
+    output_stream = stderr;
+    break;
+  case MSG_ERROR:
+    snprintf(log_prefix, sizeof(log_prefix), "%s", "Error: ");
+    output_stream = stderr;
+    break;
+  default:
+    snprintf(log_prefix, sizeof(log_prefix), "%s", "Unknown: ");
+    output_stream = stderr;
+    break;
   }
 
-  // Print severity prefix
-  if (type == MSG_INFO)
-    fprintf(stdout, "Info: ");
-  if (type == MSG_WARNING)
-    fprintf(stderr, "Warning: ");
-  else if (type == MSG_ERROR)
-    fprintf(stderr, "Error: ");
+  gettimeofday(&tv, NULL);
+  if ((tm = localtime_r(&tv.tv_sec, &tm_buf)) != NULL) {
+    strftime(without_ms, sizeof(without_ms), "%Y-%m-%d %H:%M:%S %z", tm);
+    snprintf(with_ms, sizeof(with_ms), "%s.%06ld", without_ms,
+             (long)tv.tv_usec);
+    fprintf(output_stream, "[%s] ", with_ms);
+  }
 
-  // Print the user's formatted message
-  vfprintf(stderr, fmt, args);
+  // Print the formatted message with prefix
+  fprintf(output_stream, "%s", log_prefix);
+  vfprintf(output_stream, fmt, args);
   va_end(args);
 
   // Optionally append strerror(errno)
   if (with_errno) {
-    fprintf(stderr, ": %s", strerror(errno));
+    char errbuf[128];
+    strerror_r(saved_errno, errbuf, sizeof(errbuf));
+    fprintf(output_stream, " Errno: %s", errbuf);
   }
 
-  fprintf(stderr, "\n");
+  fprintf(output_stream, "\n");
 }
