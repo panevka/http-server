abff9dcab5848ff12375d06a430b5e79f5d71a09
panevka <97028404+panevka@users.noreply.github.com>
2025-08-24 19:05:40 +0200

    feat: prevent thread-blocking when client does not close connection

--- src/main.c
+++ src/main.c
@@ -39,7 +39,21 @@ int main(void) {
 
     if (!fork()) {      // this is the child process
       close(socket_fd); // child doesn't need the listener
-      handle_request(new_socket);
+
+      int opt_result;
+      struct timeval timeout;
+      timeout.tv_sec = 10;
+      timeout.tv_usec = 0;
+
+      opt_result = setsockopt(new_socket, SOL_SOCKET, SO_RCVTIMEO, &timeout,
+                              sizeof timeout);
+
+      if (opt_result == 0) {
+        handle_request(new_socket);
+      } else if (opt_result < 0) {
+        log_msg(MSG_ERROR, true, "setsockopt has failed");
+      }
+
       close(new_socket);
       exit(0);
     }
